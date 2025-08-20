fe1ac79eaa4919936636627e2f0aa5a905695eff
rlevidev <rlevii778@gmail.com>
2025-08-19 21:42:39 -0300

    first commit

---
+++ src/main/java/com/rlevi/desafio_emprestimo/DesafioEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package com.rlevi.desafio_emprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEmprestimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlevi/desafio_emprestimo/DesafioEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlevi.desafio_emprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
